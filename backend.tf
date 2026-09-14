d64027860caf29a2bdb51bd0a3177df51caa0c61
Jurgens le Roux <jurgensleroux@outlook.com>
2026-09-14 14:14:07 +0200

    formatting

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name = "rg-tfstate"
-        storage_account_name = "sttfstateahdgal"
-        container_name = "tfstate"
-        key = "tflab.dev.tfstate"
-        use_azuread_auth = true
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstateahdgal"
+    container_name       = "tfstate"
+    key                  = "tflab.dev.tfstate"
+    use_azuread_auth     = true
+  }
 }
--- main.tf
+++ main.tf
@@ -1,40 +1,40 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 4.0"
-            }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
+  }
 }
 
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "lab" {
-    name = "rg-tflab-dev"
-    location = "southafricanorth"
+  name     = "rg-tflab-dev"
+  location = "southafricanorth"
 
-    tags = {
-        environment = "lab"
-        managed_by = "terraform"
-    }
+  tags = {
+    environment = "lab"
+    managed_by  = "terraform"
+  }
 }
 
 resource "random_string" "suffix" {
-    length = 6
-    special = false
-    upper = false
+  length  = 6
+  special = false
+  upper   = false
 }
 
 resource "azurerm_storage_account" "lab" {
-    name = "sttflab${random_string.suffix.result}"
-    resource_group_name = azurerm_resource_group.lab.name
-    location = azurerm_resource_group.lab.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+  name                     = "sttflab${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.lab.name
+  location                 = azurerm_resource_group.lab.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 output "storage_account_name" {
-    value = azurerm_storage_account.lab.name
+  value = azurerm_storage_account.lab.name
 }
